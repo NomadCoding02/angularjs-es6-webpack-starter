3ff85638f0db94cda457a17c95074f34653fa9df
Baur <youremailaddress@gmail.com>
2019-01-24 16:34:27 +0000

    First Commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+import angular from 'angular';
+
+angular.module('sse-app', []);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require("webpack-md5-hash");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: { main: './src/app.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist', {}),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash()
+    ]
+};
